98a36dcd993eda0d12f6106e7c0e41806d179420
bminczu <bminczu@gmail.com>
2021-01-06 10:42:33 -0600

    S M L buttons

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
     fetchIngredients()
-    fetchPreMadeSmoothies()
+    fetchFavSmoothies()
 })
-
+let nameForm = document.querySelector(".name-form")
 
 ////// DATA FETCHES //////
 function fetchIngredients(){
@@ -13,11 +13,11 @@ function fetchIngredients(){
     })
 }
                 
-function fetchPreMadeSmoothies(){
+function fetchFavSmoothies(){
     fetch("http://localhost:3000/smoothies")
     .then(response => response.json())
     .then(smoothies => {
-        smoothies.forEach(smoothie => showSmoothie(smoothie))
+        smoothies.forEach(smoothie => FavSmoothie(smoothie))
     })
 }
 
@@ -30,7 +30,6 @@ function showIngredient(ingredientData){
     header.innerText = ingredientData.name
     const image = document.createElement("img")
     image.src = ingredientData.photo
-    console.log(ingredientData)
     smoothieDiv.append(header, image)
     
     
@@ -39,35 +38,36 @@ function showIngredient(ingredientData){
 
 
 
-function showSmoothie(smoothieData){
+function FavSmoothie(smoothieData){
    //console.log(smoothieData)
+   if (smoothieData.favorite = true){
     const preMadeSmoothieDiv = document.querySelector(".house-favorites")
     const header = document.createElement("h1")
     header.innerText = smoothieData.name
+
     preMadeSmoothieDiv.append(header)
+   
+     }}
 
 
 
-
-}
 ///// CUSTOMER/SMOOTHIE NAME FORM //////
-// form.addEventListener("submit", event=> {
-//     event.preventDefault()
-//     let input = event.target[0].value
-//     let commentLI = document.querySelector("li")
-//     commentLI.textContent = input
-//     let commentUL = document.querySelector(".comments")
-//     commentUL.append(commentLI)
+nameForm.addEventListener("submit", event => {
+    event.preventDefault()
+    let userName = event.target[0].value
+    let smoothieName = event.target[1].value
+fetch("http://localhost:3000/smoothies", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            
+        smoothie:{
+            name: smoothieName, username: userName
+        }})
+    })
+})
+    
 
-//     let likeSpan = document.querySelector("span")
-//     let postId = parseInt(likeSpan.dataset.id, 10) 
-   
-//     fetch("http://localhost:3000/comments", {
-//         method: "POST",
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify({
-//             imageId: postId, content: input
-//         })
-//     })
-   
-// })
+
+
+
